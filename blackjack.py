fa5814eff16ddfe54ac72ddccb389e049b2f688c
Dineshbhandari2023 <iamdinesh.github@gmail.com>
2023-11-01 01:43:37 +0545

    Combo Practice

---
+++ blackjack.py
@@ -0,0 +1,73 @@
+import random
+import os
+
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def deal_card():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
+
+# this function takea list of cards and the score and calculate from the user
+def calculate_score(cards):
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+    return sum(cards)
+
+def compare(user_score, computer_score):
+    if user_score == computer_score:
+        return "Draw!!"
+    elif computer_score == 0:
+        return "Lose, Opponent has Blackjack!!" 
+    elif user_score == 0:
+        return "Win with a BlackJack!!"
+    elif user_score > 21:
+        return "You went over. You loose!!"
+    elif computer_score > computer_score:
+        return "You Win!!"
+    else:
+        return "you loose!!"
+    
+user_card = []
+computer_card = []
+is_game_over = False
+
+for _ in range(2):
+    user_card.append(deal_card())
+    computer_card.append(deal_card())
+ 
+while not is_game_over:
+    
+    user_score = calculate_score(user_card)
+    computer_score = calculate_score(computer_card)
+    print(f"Your cards; {user_card}, current score: {user_score}")
+    print(f"Computer's First card: {computer_card[0]}")
+
+
+    if user_score == 0 or computer_score == 0 or user_score >21:
+        is_game_over = True
+    else:
+        user_should_deal = input("Type 'y' to get another card, Type 'n' to pass: \n").lower()
+        if user_should_deal == "y":
+            user_card.append(deal_card())
+        else:
+            is_game_over = True
+
+while computer_card != 0 and computer_score < 17:
+      computer_card.append(deal_card())
+      computer_score = calculate_score(computer_card)
+
+
+compare(user_score, computer_score) 
+clear_console()
+
+
+
+
+
+
